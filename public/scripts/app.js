8613dbdc6a7b3092b2265620b6168da53448308c
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-22 12:08:47 -0700

    added a couple of libraries to help render html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+// function to create an individual tweet html
 function createTweetElement(tweetObj) {
   // will turn tweet obj into a tweet article
   // vars = avatar userName tagname tweet timestamp
-  <article id="handlebar-tweet">
+  let templateHtml = `<article>
     <header class="group">
      <img src={{avatar}} />
      <h2>{{userName}}</h2>
@@ -16,23 +18,62 @@ function createTweetElement(tweetObj) {
     <p>{{tweet}}</p>
 
     <footer class="group">
-     <h5>{{timestamp}} days ago</h5>
+     <h5>{{timestamp}}</h5>
      <div class="social">
       <img src="/images/icons/flag.png" />
       <img src="/images/icons/share.png" />
       <img src="/images/icons/heart.png" />
      </div>
     </footer>
-  </article>
+  </article>`;
 
+  let templateScript = Handlebars.compile(templateHtml);
 
+  let context = {
+    avatar: tweetObj.user.avatars.regular,
+    userName: tweetObj.user.name,
+    tagName: tweetObj.user.handle,
+    tweet: tweetObj.content.text,
+    timestamp: moment(tweetObj.created_at).fromNow()
+  };
 
+  return templateScript(context);
 }
 
+// Test / driver code (temporary). Eventually will get this from the server.
+var tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+var $tweet = createTweetElement(tweetData);
+$(document).ready(function(){
+  $("#tweets-container").append($tweet);
+});
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+
+
+// function to turn an array of tweet objects into tweet html
 function renderTweets(arrayOfTweets) {
+  // Get our tweets container
+  const tweetContainer = $("#tweets-container");
+
   // Takes the arrayOfTweets and appends the tweet element to the tweets-container
-  // capture #tweets-container in a jQuery object and assign to variable tweetContainer
-  // loop over array
-  // create tweet and attach to var tweet
-  // append the tweet to tweetContainer
+  arrayOfTweets.forEach(function(tweetObj){
+    const tweet = createTweetElement(tweetObj);
+    tweetContainer.append(tweet);
+  });
 }
