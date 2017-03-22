c8d41eabdad5e1697ce7dde12b0ceec5c4d28b9a
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-22 12:10:56 -0700

    testing my tweet render funcs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -40,32 +40,6 @@ function createTweetElement(tweetObj) {
   return templateScript(context);
 }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-var tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-var $tweet = createTweetElement(tweetData);
-$(document).ready(function(){
-  $("#tweets-container").append($tweet);
-});
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-
 // function to turn an array of tweet objects into tweet html
 function renderTweets(arrayOfTweets) {
   // Get our tweets container
@@ -77,3 +51,55 @@ function renderTweets(arrayOfTweets) {
     tweetContainer.append(tweet);
   });
 }
+
+// Fake data taken from tweets.json
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+$(document).ready(function() {
+  renderTweets(data);
+});
