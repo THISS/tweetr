fd92860646a5c81702135cb95f3f1880c507d8fd
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-22 08:49:35 -0700

    got the tweets looking a little more pretty

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,15 @@
 $("document").ready(function(){
-  console.log("ready");
+    const counter = $("section.new-tweet .counter");
+    const counterCount = +counter.text();
+    const textarea = $("section.new-tweet");
+  textarea.on("keyup", "textarea", function(){
+    const keyCount = $(this).val().length;
+    const count = counterCount - keyCount;
+    counter.text(count);
+    if(count < 0) {
+      counter.addClass("warning-text-count");
+    }else {
+      counter.removeClass("warning-text-count");
+    }
+  });
 });
